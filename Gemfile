022e10691d1356716884fab2987da0b8433c89a8
liam-tc <william.dyer@trunkclub.com>
2017-09-29 14:47:08 -0500

    Able to post to add to playlist

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'mongo'
+gem 'sinatra'
+gem 'spotify-client' 
+gem 'twilio-ruby', '~> 5.3.0'
---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'pp'
+require 'net/http'
+require 'twilio-ruby'
+require 'spotify-client' 
+
+# Configure Twilio
+twilio_account_sid = ENV['TWILIO_ACCOUNT_SID']
+twilio_auth_token = ENV['TWILIO_AUTH_TOKEN']
+twilio_client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
+
+# Configure Spotify
+config = {
+  :access_token => ENV['SPOTIFY_ACCESS_TOKEN'],  # initialize the client with an access token to perform authenticated calls
+  :raise_errors => true,  # choose between returning false or raising a proper exception when API calls fails
+
+  # Connection properties
+  :retries       => 0,    # automatically retry a certain number of times before returning
+  :read_timeout  => 10,   # set longer read_timeout, default is 10 seconds
+  :write_timeout => 10,   # set longer write_timeout, default is 10 seconds
+  :persistent    => false # when true, make multiple requests calls using a single persistent connection. Use +close_connection+ method on the client to manually clean up sockets
+}
+spotify_client = Spotify::Client.new(config)
+
+get '/playlist' do
+    'hello world'
+end
+
+
+post '/receive_sms' do
+    x = JSON.parse request.body.read
+    track = spotify_client.track(x.fetch("id"))
+    spotify_client.add_user_tracks_to_playlist('123031849', '5Vf9oOTnJGvfYVOrOqo2GH', uris = [track.fetch("uri")], position = nil)
+    "success maybe?"
+end
